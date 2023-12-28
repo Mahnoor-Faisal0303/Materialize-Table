45a7c6a8dab4f5012543b340755e68959886fcfd
mahnoor <faisalmahnoor74@gmail.com>
2023-12-28 15:09:30 +0500

    not found message

--- tableDeatails.js
+++ tableDeatails.js
@@ -1,7 +1,6 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 let id = urlParams.get("id");
-console.log(id);
 
 fetch(`https://fakestoreapi.com/products/${id}`)
   .then((response) => response.json())
@@ -14,3 +13,9 @@ fetch(`https://fakestoreapi.com/products/${id}`)
         <img src="${product.image}" class="images"> `;
   })
   .catch((error) => console.error("Error fetching product details:", error));
+  if(id<1 || id>20){
+    const descriptionContainer = document.getElementById(
+      "description-container"
+    );
+    descriptionContainer.innerHTML = `<p class="notFoundParagraph">Product not found</p>`
+  }
